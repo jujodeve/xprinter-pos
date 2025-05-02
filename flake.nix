21a82e200b8b51281c33071085ddb66b9d85bc20
jujodeve <jujodeve@gmail.com>
2025-05-02 20:01:39 +0000

    Create flake.nix

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  description = "CUPS drivers for Xprinter 58 & 80 thermal printers";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; # Or your preferred channel
+  };
+
+  outputs = { self, nixpkgs }:
+  let
+      pkgName = "xprinter-pos";
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+  in 
+  {
+    packages.${system}.default = pkgs.stdenv.mkDerivation {
+
+        name = pkgName;
+        version = "1.0";
+        system = "x86_64-linux";
+        src = self;
+
+        nativeBuildInputs = [
+          pkgs.autoPatchelfHook
+        ];
+
+        buildInputs = with pkgs; [
+          cups
+          glibc
+          gcc-unwrapped
+        ];
+
+        installPhase = ''
+          install -d -m 777 $out/share/cups/model/xprinterpos/
+          install -m 644 ppd/*.ppd $out/share/cups/model/xprinterpos/
+          install -m 755 -D filter/x64/rastertosnailep-pos $out/lib/cups/filter/rastertosnailep-pos
+        '';
+      };
+
+      # Overlay
+      overlays.default = final: prev: {
+        ${pkgName} = self.packages.${prev.system}.default;
+      };
+    };
+}
